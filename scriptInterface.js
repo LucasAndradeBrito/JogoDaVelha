5b4e0b2a1b02f1df644c97e0aa83c8dbde275c99
LucasAndradeBrito <99138294+LucasAndradeBrito@users.noreply.github.com>
2022-03-11 00:15:39 -0300

    Add files via upload

--- scriptInterface.js
+++ scriptInterface.js
@@ -14,46 +14,148 @@ let platePlayer2 = document.querySelector(".platePlayer2");
 let plate1 = 0;
 let plate2 = 0;
 
+let efeito = false;
+
 //Buttons
 
 buttonReset.addEventListener("click", reset);
 buttonContinuar.addEventListener("click", continuar);
 
+//Button 1: Apresentation
+
 function continuar() {
-  apresentation.style.display = "none";
-  playerConfiguration.style.display = "inline";
+  apresentation.classList = "closeEffectApresentation";
+
+  setTimeout(() => {
+    playerConfiguration.style.display = "flex";
+  }, 250);
+
+  setTimeout(() => {
+    apresentation.style.display = "none";
+  }, 500);
 }
 
+//Button 3: Alert Mensage
+
 function entendi() {
-  alertMsg.innerHTML = null;
-  alertMsg.style.display = "none";
-  playerConfiguration.style.display = "inline";
+  //
+
+  // playerConfiguration.style.display = "flex";
+  // alertMsg.innerHTML = null;
+  // alertMsg.style.display = "none";
+
+  alertMsg.classList = "closeEffectRest";
+  playerConfiguration.classList = "ReOpenEffectRest";
+
+  setTimeout(() => {
+    playerConfiguration.style.display = "flex";
+  }, 250);
+
+  setTimeout(() => {
+    alertMsg.innerHTML = null;
+    alertMsg.style.display = "none";
+  }, 500);
+
+  //
 }
+
+//Button 2: Player Configuration
 
 function comecar() {
   if (inputPlayer1.value != "" && inputPlayer2.value != "") {
-    playerConfiguration.style.display = "none";
-    game.style.display = "flex";
+    //
+
+    // playerConfiguration.style.display = "none";
+    // game.style.display = "flex";
+
+    playerConfiguration.classList = "closeEffectRest";
+    game.classList = "ReOpenEffectRest";
+
+    setTimeout(() => {
+      game.style.display = "flex";
+    }, 250);
+
+    setTimeout(() => {
+      playerConfiguration.style.display = "none";
+    }, 500);
+
+    //
   } else if (inputPlayer1.value == "" && inputPlayer2.value == "") {
-    alertMsg.innerHTML = `<h1>Atenção!</h1>
+    //
+
+    // alertMsg.innerHTML = `<h1>Atenção!</h1>
+    // <h2>Informe os nomes dos jogadores</h2>
+    // <button id="alertButton" onclick="entendi()">Entendi</button>`;
+    // alertMsg.style.display = "flex";
+    // playerConfiguration.style.display = "none";
+
+    playerConfiguration.classList = "closeEffectRest";
+    alertMsg.classList = "ReOpenEffectRest";
+
+    setTimeout(() => {
+      alertMsg.style.display = "flex";
+      alertMsg.innerHTML = `<h1>Atenção!</h1>
     <h2>Informe os nomes dos jogadores</h2>
-    <button id="alertButton" onclick="entendi()">Entendi</button>`;
-    alertMsg.style.display = "flex";
-    playerConfiguration.style.display = "none";
+    <button id="alertButton" style="margin-top: 30px" onclick="entendi()">Entendi</button>`;
+    }, 250);
+
+    setTimeout(() => {
+      playerConfiguration.style.display = "none";
+    }, 500);
+
+    //
   } else if (inputPlayer1.value == "") {
-    alertMsg.innerHTML = `
+    //
+
+    // alertMsg.innerHTML = `
+    //   <h1>Atenção!</h1>
+    //   <h2>Informe o nome do jogador 1</h2>
+    //   <button id="alertButton" onclick="entendi()">Entendi</button>`;
+    // alertMsg.style.display = "flex";
+    // playerConfiguration.style.display = "none";
+
+    playerConfiguration.classList = "closeEffectRest";
+    alertMsg.classList = "ReOpenEffectRest";
+
+    setTimeout(() => {
+      alertMsg.style.display = "flex";
+      alertMsg.innerHTML = `
       <h1>Atenção!</h1>
       <h2>Informe o nome do jogador 1</h2>
-      <button id="alertButton" onclick="entendi()">Entendi</button>`;
-    alertMsg.style.display = "flex";
-    playerConfiguration.style.display = "none";
+      <button id="alertButton" style="margin-top: 30px" onclick="entendi()">Entendi</button>`;
+    }, 250);
+
+    setTimeout(() => {
+      playerConfiguration.style.display = "none";
+    }, 500);
+
+    //
   } else {
-    alertMsg.innerHTML = `
+    //
+
+    // alertMsg.innerHTML = `
+    //   <h1>Atenção!</h1>
+    //   <h2>Informe o nome do jogador 2</h2>
+    //   <button id="alertButton" onclick="entendi()">Entendi</button>`;
+    // alertMsg.style.display = "flex";
+    // playerConfiguration.style.display = "none";
+
+    playerConfiguration.classList = "closeEffectRest";
+    alertMsg.classList = "ReOpenEffectRest";
+
+    setTimeout(() => {
+      alertMsg.style.display = "flex";
+      alertMsg.innerHTML = `
       <h1>Atenção!</h1>
       <h2>Informe o nome do jogador 2</h2>
-      <button id="alertButton" onclick="entendi()">Entendi</button>`;
-    alertMsg.style.display = "flex";
-    playerConfiguration.style.display = "none";
+      <button id="alertButton" style="margin-top: 30px" onclick="entendi()">Entendi</button>`;
+    }, 250);
+
+    setTimeout(() => {
+      playerConfiguration.style.display = "none";
+    }, 500);
+
+    //
   }
 
   //Código dos placares
